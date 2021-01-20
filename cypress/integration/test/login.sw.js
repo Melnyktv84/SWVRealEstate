b8e743c0c3b90d2ff878d2a19da630f881c9063c
David Ortiz <ortizcdavid@gmail.com>
2021-01-20 14:12:57 -0800

    Update login.sw.js

--- cypress/integration/test/login.sw.js
+++ cypress/integration/test/login.sw.js
@@ -5,14 +5,15 @@ context('Actions', () => {
     cy.visit('https://www.superworldapp.com/')
     cy.click('MuiButton-label')
     cy.wait(4000)
+    cy.contains('BUY REAL ESTATE').click()
   })
 
-  // https://on.cypress.io/interacting-with-elements
-
-  it('.type() - type into a DOM element', () => {
-    // https://on.cypress.io/type
-    cy.contains('BUY REAL ESTATE').click()
-
-  )}
+  // // https://on.cypress.io/interacting-with-elements
+  //
+  // it('.type() - type into a DOM element', () => {
+  //   // https://on.cypress.io/type
+  //   cy.contains('BUY REAL ESTATE').click()
+  //   }
+  // )
 
 )}
